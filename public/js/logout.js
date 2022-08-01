6ff9905855d318caa3e1cc7c947a41c459632aa2
dave <davidpascual200@gmail.com>
2022-07-31 20:28:32 -0700

    fixing logout button

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,19 @@
-const logout = async () => {
+// const logout = document.querySelector('#signup-button')
+console.log("hello!!")
+const logout = async (event) => {
+    event.preventDefault(); 
+    console.log('clicked')
     const response = await fetch('/api/users/logout', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
     });
-  
+    console.log("hello 2")
     if (response.ok) {
       document.location.replace('/');
     } else {
       alert(response.statusText);
     }
   };
-  
+
+//   logout.addEventListener('click', logout)
   document.querySelector('#logout').addEventListener('click', logout);
